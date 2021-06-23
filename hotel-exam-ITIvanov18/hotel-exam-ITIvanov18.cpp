0ee343e054221c8cfcd723d6fefc4bd9755db8a2
ITIvanov18 <ITIvanov18@codingburgas.bg>
2021-06-23 10:30:46 +0300

    final version

--- hotel-exam-ITIvanov18/hotel-exam-ITIvanov18.cpp
+++ hotel-exam-ITIvanov18/hotel-exam-ITIvanov18.cpp
@@ -2,87 +2,104 @@
 //
 
 #include <iostream>
-#include <string>
 #include <vector>
+#include <string>
 using namespace std;
 
-struct ROOM_PRICING
-{
-	float single = 60;
-	float double_ = 100;
-	float maisonette = 155;
-};
-
-struct BOOKING
-{
-	string clientName;
-	string roomType;
-	int nights;
-	int rentNumber;
-};
 
 class Hotel
 {
+    struct ROOM_PRICING
+    {
+        float single = 60;
+        float double_ = 100;
+        float maisonetter = 155;
+    };
+
+    struct BOOKING
+    {
+        string clientName;
+        string roomType;
+        int nights;
+        int rentNumber;
+    };
+
 public:
-	Hotel(string name, int capacity)
-	{
-		this->name = name;
-		this->capacity = capacity;
-		this->singleRooms = capacity * 0.5;
-		this->doubleRooms = capacity * 0.4;
-		this->maisonettes = capacity * 0.1;
-	}
+    Hotel(string name, int capacity)
+    {
+        this->name = name;
+        this->capacity = capacity;
+        this->singleRooms = capacity * 0.5;
+        this->doubleRooms = capacity * 0.4;
+        this->maisonette = capacity * 0.1;
+    }
+    ROOM_PRICING getRoomPricing()
+    {
+        return ROOM_PRICING();
+    }
+    string rentARoom(string clientName, string roomType, int numberOfNights)
+    {
+        bookings.push_back({
+                clientName,
+                roomType,
+                numberOfNights,
+                currentBookingNumber++
+            });
 
-	ROOM_PRICING getRoomPricing()
-	{
-		ROOM_PRICING rp;
-		return rp;
-	}
 
-	string rentARoom(string clientName, string roomType, int numberOfNights)
-	{
-		bookings.push_back({
-		clientName,
-		roomType,
-		numberOfNights,
-		currentBookingNumber++
-			});
 
-		if (roomType == "single")
-		{
-			singleRooms--;
-		}
-		if (roomType == "double")
-		{
-			doubleRooms--;
-		}
-		else
-		{
-			maisonettes--;
-		}
+        if (roomType == "single")
+        {
+            singleRooms--;
+        } if (roomType == "double")
+        {
+            doubleRooms--;
+        }
+        else
+        {
+            --maisonette;
+        }
 
-		return "";
-	}
+        return "";
+    }
+    string checkOut(int bookingNumber)
+    {
+
+    }
+
+    void report()
+    {
+        cout << name << " Database: " << endl;;
+        cout << "--------------------" << endl;
+        for (int i = 0; i < bookings.size(); i++)
+        {
+            cout << bookings[i].clientName << endl;
+            cout << bookings[i].roomType << endl;
+            cout << bookings[i].nights << endl;
+            cout << bookings[i].rentNumber << endl;
+        }
+    }
 
 private:
-	string name;
-	int capacity;
-	int singleRooms;
-	int doubleRooms;
-	int maisonettes;
-	int currentBookingNumber = 1;
-	vector<BOOKING> bookings;
+    string name;
+    int capacity;
+    vector<BOOKING>bookings;
+    int currentBookingNumber = 1;
+    int singleRooms;
+    int doubleRooms;
+    int maisonette;
 };
 
 
-void checkOut(int bookingNumber)
 
 int main()
 {
-	Hotel* hotel = new Hotel("SunnyHotel", 10);
-	hotel->rentARoom("Peter", "single", 4);
-	hotel->report();
-}
+    Hotel* hotel = new Hotel("SunnyHotel", 10);
+    hotel->rentARoom("Peter", "single", 4);
+    hotel->report();
+
+
+};
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
